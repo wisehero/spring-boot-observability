2426e6e71943add4e3d803a06b3494d67d79778e
wisehero <godjw96@gmail.com>
2024-11-26 20:11:41 +0900

    Initial commit

---
+++ src/test/java/wisehero/springbootobservability/SpringBootObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package wisehero.springbootobservability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootObservabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
