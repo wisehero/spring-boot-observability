7ff8879a447c777f63430c4e62e2ae0da8019567
wisehero <godjw96@gmail.com>
2024-11-30 23:38:36 +0900

    Filter 순서 제거

--- app/src/main/java/wisehero/springbootobservability/config/FilterConfig.java
+++ app/src/main/java/wisehero/springbootobservability/config/FilterConfig.java
@@ -3,7 +3,6 @@ package wisehero.springbootobservability.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 
 import wisehero.springbootobservability.filter.HTTPLoggingFilter;
 
@@ -15,7 +14,6 @@ public class FilterConfig {
 		FilterRegistrationBean<HTTPLoggingFilter> registrationBean = new FilterRegistrationBean<>();
 		HTTPLoggingFilter apiLoggingFilter = new HTTPLoggingFilter();
 		registrationBean.setFilter(apiLoggingFilter);
-		registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		registrationBean.addUrlPatterns("/api/*");
 		return registrationBean;
 	}
