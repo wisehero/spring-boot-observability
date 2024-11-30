35fb432e4dd9f604bf86cd4d4dab7d83186317fe
wisehero <godjw96@gmail.com>
2024-11-30 10:03:24 +0900

    MethodArgumentException 테스트를 위한 검증 어노테이션 설정

--- src/main/java/wisehero/springbootobservability/hello/request/ExampleRequestBody.java
+++ src/main/java/wisehero/springbootobservability/hello/request/ExampleRequestBody.java
@@ -1,7 +1,12 @@
 package wisehero.springbootobservability.hello.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record ExampleRequestBody(
+	@NotBlank(message = "이름은 필수입니다.")
 	String name,
+	@NotNull(message = "나이는 필수입니다.")
 	Integer age,
 	String description
 ) {
